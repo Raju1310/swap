3749382fb3d225ea7d8e2a1949b03db50a3fd904
Raju1310 <Rajuespi13@gmail.com>
2022-09-23 22:31:48 +0530

    trial

--- Swap.java
+++ Swap.java
@@ -11,10 +11,11 @@ void main(){
     a=sc.nextInt();
     System.out.println("enter B value:");
     b=sc.nextInt();
-    System.out.println("A,B value are: "+a+","+b);
+    System.out.println("before swap A,B value are: "+a+","+b);
     a=a+b;
     b=a-b;
     a=a-b;
+    System.out.println("after swap A,B value are: "+a+","+b);
     
 
 }
