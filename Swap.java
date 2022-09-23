d875ed29f60b2674ca6982f192a9fc7a5cd8b33f
Raju1310 <Rajuespi13@gmail.com>
2022-09-23 22:29:25 +0530

    trial

--- Swap.java
+++ Swap.java
@@ -1,1 +1,22 @@
+
 import java.io.*;
+import java.util.*;
+public class Swap{
+
+void main(){
+
+    int a,b;
+    Scanner sc=new Scanner(System.in);
+    System.out.println("enter A value:");
+    a=sc.nextInt();
+    System.out.println("enter B value:");
+    b=sc.nextInt();
+    System.out.println("A,B value are: "+a+","+b);
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    
+
+}
+
+}
